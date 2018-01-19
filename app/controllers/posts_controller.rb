cc067b200f4a2bc4e8879c6152dd17b15ff0ed26
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-19 12:14:56 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.valid?
     @post.update(post_params)
+    if @post.valid?
     @post.save
     redirect_to post_path(@post)
   else
